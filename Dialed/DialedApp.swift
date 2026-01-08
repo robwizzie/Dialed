218cdaae937827f2a593fa32106d906ee1297e99
robwizzie <robertwiscount@gmail.com>
2026-01-07 23:06:19 -0500

    Initial Commit

---
+++ Dialed/DialedApp.swift
@@ -0,0 +1,17 @@
+//
+//  DialedApp.swift
+//  Dialed
+//
+//  Created by Robert Wiscount on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct DialedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DialedTests/DialedTests.swift
@@ -0,0 +1,17 @@
+//
+//  DialedTests.swift
+//  DialedTests
+//
+//  Created by Robert Wiscount on 1/7/26.
+//
+
+import Testing
+@testable import Dialed
+
+struct DialedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
